38fe64e2eb55c7119f77c26e15cf8c73141e4719
KachikovPetko <123257480+KachikovPetko@users.noreply.github.com>
2023-05-04 11:46:23 +0300

    Add files via upload

---
+++ Methods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void GoodBy(string Name)
+        {
+            Console.WriteLine("Good by, " + Name + "!");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string Name = Console.ReadLine();
+            GoodBy(Name);
+        }
+    }
+}
